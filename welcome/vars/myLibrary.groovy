917eaa8bca0df8a0d06a430ed657fc92f7f94518
HenCohen888 <hencohen@gmail.com>
2026-05-13 19:57:51 +0300

    Add shared library structure

---
+++ welcome/vars/myLibrary.groovy
@@ -0,0 +1,11 @@
+def buildApp() {
+    echo "Building Application from Shared Library..."
+}
+
+def deployApp() {
+    echo "Deploying Application from Shared Library..."
+}
+
+def cleanup() {
+    echo "Cleaning up from Shared Library..."
+}
